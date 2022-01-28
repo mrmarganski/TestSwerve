0ef1682a0c136eb1aa223a9ecb17d880d70107db
mrmarganski <bmarganski@region15.org>
2022-01-27 22:28:23 -0500

    In Progress

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -42,6 +42,7 @@ public final class Constants {
         public static final int kFrontRightTurningMotorPort = 2;
         public static final int kBackRightTurningMotorPort = 5;
 
+        //Internal Encoder for NEO Turning Motor
         public static final boolean kFrontLeftTurningEncoderReversed = true;
         public static final boolean kBackLeftTurningEncoderReversed = true;
         public static final boolean kFrontRightTurningEncoderReversed = true;
